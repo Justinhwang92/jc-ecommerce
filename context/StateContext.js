12e2ab22aa4785b35bb18d06d21fb95eeba5081d
Justin <justinhwang92@gmail.com>
2022-06-25 12:44:00 -0400

    Add context for cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -7,7 +7,7 @@ export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
   // logic for Add to Cart button
@@ -59,6 +59,7 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
