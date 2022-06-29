db2fb7f20108cc230bdd3807d78d83140a0cb0ff
Justin <justinhwang92@gmail.com>
2022-06-28 20:43:37 -0400

    Add stripe details

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -11,12 +11,36 @@ export default async function handler(req, res) {
         payment_method_types: ["card"],
         billing_address_collection: "auto",
         shipping_options: [
-          { shipping_rate: "shr_1LF3ZLGnwCRQsN2s4fMRZTmv" },
-          { shipping_rate: "shr_1LF3ZvGnwCRQsN2soOuPsGyu" },
+          { shipping_rate: "shr_1LFokLGnwCRQsN2s6fEOA9WE" },
+          { shipping_rate: "shr_1LFokiGnwCRQsN2sQ08oOOWr" },
         ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/znwfoctn/production/"
+            )
+            .replace("-webp", ".webp");
 
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
       };
 
       // Create Checkout Sessions from body params.
